c6c0bbe1bd96b21aeecb0af33a3fe4d7e7e97e66
kamil <kamil@192.168.1.12>
2019-09-22 18:20:54 -0500

    Iterator fix

--- Iterator/src/Menu.java
+++ Iterator/src/Menu.java
@@ -1,41 +1,25 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
-public class Menu {
+public class Menu implements Container {
 
-	List<Item> menuItems;
+	public String items[] = {"Pizza" , "Cheeseburger" , "Hamburger", "Turkey Sandwich", "Ice Cream" , "Tacos"};
 	
-	public Menu() {
-		menuItems = new ArrayList<Item>();
+	public Iterator getIterator() { 
+		return new ItemIterator();
 	}
 	
-	public void addItem(Item item) {
-		menuItems.add(item);
-	}
-	
-	public Iterator<Item> iterator() {
-		return new MenuIterator();
-	}
-	
-	class MenuIterator implements Iterator<Item> {
-		int currentIndex = 0;
-		
+	private class ItemIterator implements Iterator { 
+		int index;
 		public boolean hasNext() {
-			if (currentIndex >= menuItems.size()) {
-				return false;}
-			else {
+			if(index < items.length) {
 				return true;
 			}
+			return false;
 		}
-		
-		public Item next() {
-			return menuItems.get(currentIndex++);
-					
+		public Object next() {
+			if(this.hasNext()) {
+				return items[index++];
 			}
-		
-		public void remove() {
-			menuItems.remove(--currentIndex);	
+			return null;
 		}
 	}
 }
--- Iterator/src/Test.java
+++ Iterator/src/Test.java
@@ -1,32 +1,12 @@
-import java.util.Iterator;
 
 public class Test {
 
 	public static void main(String[] args) {
-		Item f1 = new Item("chocolate", 2.55f);
-		Item f2 = new Item("Vanilla", 1.55f);
-		Item f3 = new Item("Red Velvet", 3.55f);
-		Item f4 = new Item("Caramel", 3.75f);		
-	
-		Menu menu = new Menu();
-		menu.addItem(f1);
-		menu.addItem(f2);
-		menu.addItem(f3);
-		menu.addItem(f4);
-		System.out.println("Displaying Menu : " );
-		Iterator<Item> iterator = menu.iterator();
-		while(iterator.hasNext()) {
-			Item item = iterator.next();
-			System.out.println(item);
-		}
-		System.out.println ("Removing last item returned");
-		iterator.remove();
+		Menu itemsList = new Menu();
 		
-		System.out.println("Displaying Menu:");
-		iterator = menu.iterator();
-		while(iterator.hasNext()) {
-			Item item = iterator.next();
-			System.out.println(item);
+		for(Iterator iter = itemsList.getIterator(); iter.hasNext();) {
+			String name = (String)iter.next();
+			System.out.println("Item: " + name);
 		}
 	}
 }
